ead8c5eb6dac2dedd5c0e7d05c7221308888d97c
Martinho Gonçalves <martinhogon@gmail.com>
2022-06-28 11:56:49 +0100

    Scrit Funcional

--- MoverDiretorios.ps1
+++ MoverDiretorios.ps1
@@ -11,7 +11,7 @@ param(
     # [switch]$MoverServicos # Este parametro pode ser irelevante
 )
 
-function CreateFolder () {
+function CreateFolders () {
     param(
         [Parameter(Mandatory = $true)] $jsonObject #pastas
     )
@@ -28,6 +28,8 @@ function CreateFolder () {
             else {
                 New-Item -Path $pasta.destination -Name $pasta.name -ItemType "directory" -ErrorAction Stop | Out-Null
                 Write-Host "Foi criada com sucesso a pasta: $folderName" -ForegroundColor Green
+                #Vai atribuir as permissões á pasta
+                AtribuirPermissoes -jsonObject $pasta.permissions -FolderPath $newFolder -folderName $pasta.name
             }
         }
         catch {
@@ -44,38 +46,62 @@ function CreateFolder () {
     }
 }
 
-function AtribuirPermissoes () {
+function MoveFolders {
     param(
-        [Parameter(Mandatory = $true)] $jsonObject, #permissoes
-        [String] $Destination,
-        [String] $folderName
+        [Parameter(Mandatory = $true)] $jsonObject #pastas
     )
     $erro = 0
-    Write-Host "A atribuir permissoes á pasta $folderName...."
-    try {
-        $ACL = Get-Acl $Destination
-        foreach($permissao in $jsonObject){
-            $AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule($oermissao.utilizador, $permissao.permissao, "ContainerInherit,ObjectInherit", "None", "Allow")
-            $ACL.SetAccessRule($AccessRule)
+    $msgErro = "Ocorreu um erro ao mover as seguintes pastas:"
+    Write-Host "A mover pastas..."
+    foreach ($pasta in $jsonObject) {
+        try {
+            Write-Host "A mover a pasta: $folderName"
+            Move-Item -Path $pasta.path -Destination $pasta.destination -ErrorAction Stop
+            Write-Host "Pasta movida com sucesso!!" -ForegroundColor Green 
+            $newFolder = $pasta.destination+$pasta.name
+            AtribuirPermissoes -jsonObject $pasta.permissions -FolderPath $newFolder -folderName $pasta.name
         }
-        $ACL | Set-Acl -Path $Destination -ErrorAction Stop
-    }
-    catch {
+        catch {
+            Write-Host "Ocorreu um erro ao mover a pasta "+$pasta.name -ForegroundColor Red
+            $erro = 1
+            $msgErro += " $folderName ||"
+        }
     }
 
     if ($erro -eq 0) {
-        Write-Host "Todas as permissões foram atribuidas com sucesso!" -ForegroundColor Green
+        Write-Host "Todas as pastas foram movidas com sucesso!!" -ForegroundColor Green
     }
     else {
         Write-Host $msgErro -ForegroundColor Red
     }
+}
+
+function AtribuirPermissoes () {
+    param(
+        [Parameter(Mandatory = $true)] $jsonObject, #permissoes
+        [String] $FolderPath,
+        [String] $folderName
+    )
+    # $erro = 0
+    # $msgErro = "Ocorreu um erro ao atribuir permissoes as seguintes pastas:"
+    Write-Host "A atribuir permissoes a pasta $folderName...."
+    try {
+        $ACL = Get-Acl $FolderPath
+        foreach ($permissao in $jsonObject) {
+            write-host $permissao.utilizador " - " $permissao.permissao
+            $AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule($permissao.utilizador, $permissao.permissao, "ContainerInherit,ObjectInherit", "None", "Allow")
+            $ACL.SetAccessRule($AccessRule)
+        }
+        $ACL | Set-Acl -Path $FolderPath #-ErrorAction Stop
+    }
+    catch {
+        Write-Host "Ocorreu um erro ao atribuir permissoes a pasta: $folderName" -ForegroundColor Red
+    }
 
 }
 
-# $erroPermissoes = 0
-# $msgErroPermissoes = "Ocorreu um erro ao atribuir as seguintes permissões:"
-
-# Write-Host $json.criar[0].name
-
 $json = Get-Content $JSONFile -Encoding utf8 | ConvertFrom-Json
-CreateFolder -jsonObject $json.criar
+Write-Host "Pastas a criar: "$json.criar.Count
+Write-Host "Pastas a mover: "$json.mover.Count
+CreateFolders -jsonObject $json.criar
+MoveFolders -jsonObject $json.mover
